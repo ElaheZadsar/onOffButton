5af7254fe7ba4a5df0f71315026a2aa2076f8c49
Elahe Zadsar <Elahezadsar81@gmail.com>
2024-08-06 13:07:39 +0330

    button

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-import './stylesheet.scss';
+// import './stylesheet.scss';
+
+function showContainer2() {
+    document.getElementById('container').style.display = 'none';
+    document.getElementById('container2').style.display = 'block';
+}
+
+function showContainer1() {
+    document.getElementById('container').style.display = 'block';
+    document.getElementById('container2').style.display = 'none';
+}
+
+document.addEventListener("click", showContainer2);
+document.addEventListener("click", showContainer1);
